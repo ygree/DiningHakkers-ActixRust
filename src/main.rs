4988ab5da3b0e5132b087dc6bcf099594373959b
Yury Gribkov <yury.gribkov@gmail.com>
2018-11-25 14:10:58 -0500

    Start implementing Hakker logic. Getting hungry in 5 seconds and trying to obtain chopsticks.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 extern crate actix;
 extern crate futures;
 
-use actix::dev::MessageResponse;
-use actix::dev::ResponseChannel;
-use actix::{msgs, Actor, Addr, Arbiter, Context, Handler, Message, Recipient, System};
+use actix::dev::*;
+// use actix::prelude::*;
 use futures::{future, Future};
+use std::time::Duration;
+// use std::fmt::Error;
 
 enum Chopstick {
     Available,
@@ -42,14 +43,14 @@ impl Handler<ChopstickMessage> for Chopstick {
                 ChopstickMessage::Put(ref sender) if sender == hakker => {
                     (Some(Chopstick::Available), ChopstickAnswer::PutBack)
                 }
-                _ => unimplemented!("Can't be put back by another hakker"),
+                _ => unreachable!("Chopstick can't be put back by another hakker"),
             },
             // When a Chopstick is available, it can be taken by a hakker
             Chopstick::Available => match msg {
                 ChopstickMessage::Take(hakker) => {
                     (Some(Chopstick::TakenBy(hakker)), ChopstickAnswer::Taken)
                 }
-                _ => unimplemented!("It's not taken"),
+                _ => unreachable!("Chopstick isn't taken"),
             },
         };
         if let Some(ns) = new_state {
@@ -75,11 +76,94 @@ where
     }
 }
 
-struct Hakker;
+struct Hakker {
+    name: String,
+    left: Addr<Chopstick>,
+    right: Addr<Chopstick>,
+    state: HakkerState,
+}
+
+enum HakkerState {
+    Waiting,
+    Thinking,
+    Hungry,
+    WaitingForOtherChopstick(Addr<Chopstick>),
+}
 
 impl Actor for Hakker {
     type Context = Context<Self>;
 }
+
+enum HakkerMessage {
+    Eat,
+    Think,
+}
+
+impl Handler<HakkerMessage> for Hakker {
+    type Result = ();
+
+    fn handle(&mut self, msg: HakkerMessage, ctx: &mut Self::Context) -> Self::Result {
+        let (new_state, resp) = match self.state {
+            HakkerState::Waiting => match msg {
+                HakkerMessage::Think => {
+                    println!("{} starts to think", self.name);
+                    let five_seconds = Duration::new(5, 0);
+                    ctx.notify_later(HakkerMessage::Eat, five_seconds);
+                    (Some(HakkerState::Thinking), ())
+                }
+                _ => unreachable!("From waiting state Hakker can only start to think."),
+            },
+            // When a hakker is thinking it can become hungry
+            // and try to pick up its chopsticks and eat
+            HakkerState::Thinking => match msg {
+                HakkerMessage::Eat => {
+                    println!("start eating");
+                    self.left
+                        .send(ChopstickMessage::Take(ctx.address()))
+                        .into_actor(self)
+                        .then(|res, act, ctx| {
+                            println!("getting response from the left chopstick: {:?}", res);
+                            //TODO can change state right here
+                            // act.state = HakkerState::WaitingForOtherChopstick(act.left.clone());
+
+                            //TODO can send a message to itself
+                            // ctx.address()
+                            //     .send(HakkerMessage::Eat)
+                            //     .into_actor(act)
+                            //     .then(|r, a, c| actix::fut::ok(()))
+
+                            actix::fut::ok(())
+                        }).spawn(ctx);
+
+                    (Some(HakkerState::Hungry), ())
+                }
+                _ => unimplemented!("In thinking mode it can only handle Eat messag"),
+            },
+
+            _ => unimplemented!(), //TODO
+        };
+        if let Some(ns) = new_state {
+            self.state = ns;
+        }
+        resp
+    }
+}
+
+impl Message for HakkerMessage {
+    type Result = (); //TODO
+}
+
+// impl<A, M> MessageResponse<A, M> for ChopstickAnswer
+// where
+//     A: Actor,
+//     M: Message<Result = ChopstickAnswer>,
+// {
+//     fn handle<R: ResponseChannel<M>>(self, ctx: &mut <A as Actor>::Context, tx: Option<R>) {
+//         if let Some(tx) = tx {
+//             tx.send(self);
+//         }
+//     }
+// }
 
 // struct Ping;
 
@@ -120,19 +204,27 @@ impl Actor for Hakker {
 fn main() {
     let system = actix::System::new("test");
 
-    let chopstick = Chopstick::Available.start();
-    let hakker = Hakker.start();
+    let chopstick1 = Chopstick::Available.start();
+    let chopstick2 = Chopstick::Available.start();
+    let hakker = Hakker {
+        name: "Yury".to_owned(),
+        left: chopstick1.clone(),
+        right: chopstick2.clone(),
+        state: HakkerState::Waiting,
+    }.start();
+
+    let req = hakker.send(HakkerMessage::Think);
 
     // let resp = chopstick.send(ChopstickMessage::Put(hakker));
-    let resp = chopstick.send(ChopstickMessage::Take(hakker));
+    // let resp = chopstick1.send(ChopstickMessage::Take(hakker.clone()));
 
-    Arbiter::spawn(resp.then(|resp| {
+    Arbiter::spawn(req.then(|resp| {
         match resp {
             Ok(r) => println!("resp: {:?}", r),
-            _ => println!("error")
+            _ => println!("error"),
         }
-        
-        System::current().stop();
+
+        // System::current().stop();
         future::result(Ok(()))
     }));
 
