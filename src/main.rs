8dd987d723b56f5a1614b023594374518ab7abd9
Yury Gribkov <yury.gribkov@gmail.com>
2018-11-25 15:34:05 -0500

    Implement happy-path scenario of getting chopstick when they are available.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use futures::{future, Future};
 use std::time::Duration;
 // use std::fmt::Error;
 
+#[derive(Debug)]
 enum Chopstick {
     Available,
     TakenBy(Addr<Hakker>),
@@ -25,7 +26,7 @@ enum ChopstickMessage {
 
 #[derive(Debug)]
 enum ChopstickAnswer {
-    Taken,
+    Taken(Addr<Chopstick>),
     Busy,
     PutBack,
 }
@@ -47,9 +48,10 @@ impl Handler<ChopstickMessage> for Chopstick {
             },
             // When a Chopstick is available, it can be taken by a hakker
             Chopstick::Available => match msg {
-                ChopstickMessage::Take(hakker) => {
-                    (Some(Chopstick::TakenBy(hakker)), ChopstickAnswer::Taken)
-                }
+                ChopstickMessage::Take(hakker) => (
+                    Some(Chopstick::TakenBy(hakker)),
+                    ChopstickAnswer::Taken(ctx.address()),
+                ),
                 _ => unreachable!("Chopstick isn't taken"),
             },
         };
@@ -76,6 +78,7 @@ where
     }
 }
 
+#[derive(Debug)]
 struct Hakker {
     name: String,
     left: Addr<Chopstick>,
@@ -83,11 +86,13 @@ struct Hakker {
     state: HakkerState,
 }
 
+#[derive(Debug)]
 enum HakkerState {
     Waiting,
     Thinking,
     Hungry,
     WaitingForOtherChopstick(Addr<Chopstick>),
+    Eating,
 }
 
 impl Actor for Hakker {
@@ -97,6 +102,46 @@ impl Actor for Hakker {
 enum HakkerMessage {
     Eat,
     Think,
+}
+
+impl Handler<ChopstickAnswer> for Hakker {
+    type Result = ();
+
+    fn handle(&mut self, msg: ChopstickAnswer, ctx: &mut Self::Context) -> Self::Result {
+        let new_state = match msg {
+            ChopstickAnswer::Taken(chopstick) => match self.state {
+                HakkerState::Hungry => {
+                    let waiting_on = if self.left == chopstick {
+                        self.right.clone()
+                    } else if self.right == chopstick {
+                        self.left.clone()
+                    } else {
+                        unreachable!("Received unknown chopstick: {:?}", chopstick)
+                    };
+                    println!("taken first chopstick: {:?}", chopstick);
+                    Some(HakkerState::WaitingForOtherChopstick(waiting_on))
+                },
+                HakkerState::WaitingForOtherChopstick(ref waiting_on) if waiting_on == &chopstick => {
+                    println!("{} has picked up {:?} and {:?} and starts to eat", self.name, self.left, self.right);
+
+                    let five_seconds = Duration::new(5, 0);
+                    ctx.notify_later(HakkerMessage::Think, five_seconds);
+
+                    Some(HakkerState::Eating)
+                }
+                _ => unimplemented!("TODO"),
+            },
+            ChopstickAnswer::Busy => unimplemented!("TODO"),
+            _ => unimplemented!("TODO"),
+        };
+        if let Some(ns) = new_state {
+            self.state = ns;
+        }
+    }
+}
+
+impl Message for ChopstickAnswer {
+    type Result = ();
 }
 
 impl Handler<HakkerMessage> for Hakker {
@@ -122,22 +167,40 @@ impl Handler<HakkerMessage> for Hakker {
                         .send(ChopstickMessage::Take(ctx.address()))
                         .into_actor(self)
                         .then(|res, act, ctx| {
-                            println!("getting response from the left chopstick: {:?}", res);
-                            //TODO can change state right here
-                            // act.state = HakkerState::WaitingForOtherChopstick(act.left.clone());
-
-                            //TODO can send a message to itself
-                            // ctx.address()
-                            //     .send(HakkerMessage::Eat)
-                            //     .into_actor(act)
-                            //     .then(|r, a, c| actix::fut::ok(()))
-
-                            actix::fut::ok(())
+                            match res {
+                                Ok(m) => {
+                                    println!("getting response from the left chopstick: {:?}", m);
+                                    ctx.address()
+                                        .send(m)
+                                        .into_actor(act)
+                                        .then(|r, a, c| actix::fut::ok(()))
+                                }
+                                _ => unimplemented!(), // actix::fut::ok(()) //ignore
+                            }
+                        }).spawn(ctx);
+                    self.right
+                        .send(ChopstickMessage::Take(ctx.address()))
+                        .into_actor(self)
+                        .then(|res, act, ctx| {
+                            match res {
+                                Ok(m) => {
+                                    println!("getting response from the right chopstick: {:?}", m);
+                                    ctx.address()
+                                        .send(m)
+                                        .into_actor(act)
+                                        .then(|r, a, c| actix::fut::ok(()))
+                                }
+                                _ => unimplemented!(), // actix::fut::ok(()) //ignore
+                            }
                         }).spawn(ctx);
 
                     (Some(HakkerState::Hungry), ())
                 }
                 _ => unimplemented!("In thinking mode it can only handle Eat messag"),
+            },
+
+            HakkerState::Eating => {
+                unimplemented!("TODO: eating when receive Think put back chopsticks and start thinking")
             },
 
             _ => unimplemented!(), //TODO
